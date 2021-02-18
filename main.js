c4a77652dea86ffdd69a5e90cd7d65920b62da58
Laura Caldaronello <caldaronello.l@gmail.com>
2021-02-18 13:44:21 +0100

    Esercizio ok, penso a bonus

---
+++ main.js
@@ -0,0 +1,7 @@
+//esercizio
+
+var firstName = prompt('Scrivi il tuo nome:');
+var lastName = prompt('Scrivi il tuo cognome:');
+var favColour = prompt('Scrivi il tuo colore preferito:');
+
+document.getElementById('psw-1').innerHTML = firstName + lastName + favColour + 19;
